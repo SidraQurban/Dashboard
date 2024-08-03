a2da4d6ace98f3802ea80e0f95592734089da372
SidraQurban <sidraqurban77@gmail.com>
2024-08-04 00:40:54 +0500

    ./

---
+++ App.js
@@ -0,0 +1,9 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet } from "react-native";
+import AppNavigator from "./app/navigation/AppNavigator";
+
+export default function App() {
+  return <AppNavigator />;
+}
+
+const styles = StyleSheet.create({});
---
+++ app/navigation/AppNavigator.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Login from "../screens/Login";
+
+const Stack = createNativeStackNavigator();
+const AppNavigator = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="login" component={Login} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default AppNavigator;

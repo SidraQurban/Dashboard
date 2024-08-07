5c268a52a6b115bfd56560e0c4499520db6bd46b
SidraQurban <sidraqurban77@gmail.com>
2024-08-07 22:17:08 +0500

    //

--- app/components/Header.js
+++ app/components/Header.js
@@ -5,116 +5,71 @@ import { EvilIcons, Entypo, FontAwesome6 } from "react-native-vector-icons";
 const Header = () => {
   return (
     <>
-      <View style={styles.header}>
-        <TouchableOpacity style={styles.menuIcon}>
-          <Entypo name="menu" size={23} color="black" />
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.userIcon}>
-          <EvilIcons name="user" size={34} color="white" />
-        </TouchableOpacity>
-      </View>
-      <View style={styles.welcomeContainer}>
-        <View style={styles.profileIcon}>
+      <View
+        style={{
+          width: "100%",
+          height: 200,
+          backgroundColor: "#234d51",
+          flexDirection: "row",
+          padding: 20,
+          position: "relative",
+        }}
+      >
+        <View
+          style={{
+            backgroundColor: "#9999a1",
+            borderRadius: 30,
+            width: 55,
+            height: 55,
+            justifyContent: "center",
+            alignItems: "center",
+            marginLeft: 20,
+            top: 25,
+          }}
+        >
           <FontAwesome6 name="user-large" size={30} color="white" />
         </View>
-        <View style={styles.textContainer}>
-          <Text style={styles.welcomeText}>Welcome back, Umer!</Text>
-          <View style={styles.bellContainer}>
+        <View style={{ marginLeft: 15, flex: 1 }}>
+          <Text
+            style={{ color: "#fff", fontSize: 20, fontWeight: "bold", top: 17 }}
+          >
+            Welcome back, Umer!
+          </Text>
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              marginTop: 5,
+              top: 15,
+            }}
+          >
             <EvilIcons
               name="bell"
               size={25}
               color="#ced4da"
-              style={styles.bellIcon}
+              style={{ marginTop: 5 }}
             />
-            <Text style={styles.superadminText}>superadmin</Text>
+            <Text style={{ color: "#ced4da", marginLeft: 5 }}>superadmin</Text>
           </View>
         </View>
-        <View style={styles.sgpContainer}>
-          <Text style={styles.sgpText}>SGP Technologies</Text>
+        <View
+          style={{
+            backgroundColor: "#abb2b7",
+            height: 45,
+            width: "50%",
+            alignItems: "center",
+            justifyContent: "center",
+            borderTopLeftRadius: 20,
+            position: "absolute",
+            bottom: 0,
+            left: 40,
+          }}
+        >
+          <Text style={{ color: "#fff", fontSize: 15 }}>SGP Technologies</Text>
         </View>
       </View>
     </>
   );
 };
-
-const styles = StyleSheet.create({
-  header: {
-    width: "100%",
-    height: 50,
-    backgroundColor: "#fff",
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    paddingHorizontal: 1,
-    position: "relative",
-  },
-  menuIcon: {
-    padding: 10,
-  },
-  userIcon: {
-    backgroundColor: "#9999a1",
-    borderRadius: 30,
-    width: 40,
-    height: 40,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  welcomeContainer: {
-    width: "100%",
-    height: 200,
-    backgroundColor: "#234d51",
-    flexDirection: "row",
-    padding: 20,
-    position: "relative",
-  },
-  profileIcon: {
-    backgroundColor: "#9999a1",
-    borderRadius: 30,
-    width: 55,
-    height: 55,
-    justifyContent: "center",
-    alignItems: "center",
-    marginLeft: 20,
-    top: 25,
-  },
-  textContainer: {
-    marginLeft: 15,
-    flex: 1,
-  },
-  welcomeText: {
-    color: "#fff",
-    fontSize: 20,
-    fontWeight: "bold",
-    top: 17,
-  },
-  bellContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    marginTop: 5,
-    top: 15,
-  },
-  bellIcon: {
-    marginTop: 5,
-  },
-  superadminText: {
-    color: "#ced4da",
-    marginLeft: 5,
-  },
-  sgpContainer: {
-    backgroundColor: "#abb2b7",
-    height: 45,
-    width: "50%",
-    alignItems: "center",
-    justifyContent: "center",
-    borderTopLeftRadius: 20,
-    position: "absolute",
-    bottom: 0,
-    left: 40,
-  },
-  sgpText: {
-    color: "#fff",
-    fontSize: 15,
-  },
-});
 
 export default Header;
--- app/screens/Dashboard.js
+++ app/screens/Dashboard.js
@@ -2,11 +2,14 @@ import { View, Text, ScrollView } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Header from "../components/Header";
+import Drawer from "../screens/Drawer";
 
 const Dashboard = () => {
   return (
     <SafeAreaView>
       <ScrollView>
+        <Drawer />
+
         <Header />
         {/* <Sidebar /> */}
         <Text>Sidebar</Text>
